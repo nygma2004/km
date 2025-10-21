387efc11ea4dcb776cb1b00c1ecf8451e4e9e149
Csongor Varga <nygma2004@hotmail.com>
2025-10-21 21:05:25 +0200

    Add files via upload

---
+++ shelly_lora_examples/lora_virtual_switch_sender.js
@@ -0,0 +1,92 @@
+//AES key is only for example, generate unique key!!
+const aesKey = '2664ed9a5fac9b03164bc2d57b339644d08e360e99a28fc05b307d6e15d085a5';
+const CHECKSUM_SIZE = 4;
+
+function encryptMessage(msg, keyHex) {
+  function fromHex(hex) {
+    const arr = new ArrayBuffer(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+      arr[i / 2] = parseInt(hex.substr(i, 2), 16);
+    }
+    return arr;
+  }
+
+  function padRight(msg, blockSize) {
+    const paddingSize = (blockSize - msg.length % blockSize) % blockSize;;
+
+    for (let i = 0; i < paddingSize; i++) {
+      msg += ' ';
+    }
+
+    return msg;
+  }
+
+  msg = msg.trim();
+  const formattedMsg = padRight(msg, 16);
+  const key = fromHex(keyHex);
+  const encMsg = AES.encrypt(formattedMsg, key, { mode: 'ECB' });
+  return encMsg;
+}
+
+function generateChecksum(msg) {
+  let checksum = 0;
+  for (let i = 0; i < msg.length; i++) {
+    checksum ^= msg.charCodeAt(i);
+  }
+  let hexChecksum = checksum.toString(16);
+
+  while (hexChecksum.length < CHECKSUM_SIZE) {
+    hexChecksum = '0' + hexChecksum;
+  }
+
+  return hexChecksum.slice(-CHECKSUM_SIZE);
+}
+
+function sendMessage(message) {
+  const checkSumMessage = generateChecksum(message) + message;
+  const encryptedMessage = encryptMessage(checkSumMessage, aesKey);
+
+  Shelly.call(
+    'Lora.SendBytes',
+    { id: 100, data: btoa(encryptedMessage) },
+    function (_, err_code, err_msg) {
+      if (err_code !== 0) {
+        console.log('Error:', err_code, err_msg);
+      }
+    }
+  );
+}
+
+Shelly.addEventHandler(function (event) {
+  // for debugging
+  //console.log("Event: ",event);
+  
+  // Virtual button 200 is pressed
+  if (
+    typeof event === 'object' &&
+    event.event === 'single_push' &&
+    event.component === "button:200"
+  ) {
+    sendMessage("AO");
+    console.log("Gate opening message sent");
+  }
+
+
+});
+
+Shelly.addStatusHandler(function (status) {
+  // for debugging
+  //console.log("Status: ",status);
+ 
+  // Garden light switch was operated
+  if (status.component === "boolean:200") {
+    if (status.delta.value) {
+      sendMessage("BO");
+      console.log("Garden light on message sent");
+    } else {
+      sendMessage("BF");
+      console.log("Garden light off message sent");
+    }
+  }
+
+});
